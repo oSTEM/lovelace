f57f5b6091d5c88405e6443ef0b7d252b74b1595
Janine vN <janine9vn@gmail.com>
2020-11-24 22:39:38 -0500

    Update bot/utils/role_checks.py
    Co-authored-by: Travis DePrato <773453+travigd@users.noreply.github.com>

--- bot/utils/role_checks.py
+++ bot/utils/role_checks.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 
 
-async def _has_role_check(ctx: commands.Context, guild, *role_name: int) -> bool:
+async def _has_role_check(ctx: commands.Context, guild, *role_names: str) -> bool:
     """Checks if the user has a specific role within the server"""
     member = guild.get_member(int(ctx.author.id))
 
